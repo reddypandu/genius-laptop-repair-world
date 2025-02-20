99fdb501f9b5fa7332650b6e021f3f6e266496a5
Patternscompany <pandureddypatterns@gmail.com>
2025-02-20 22:40:29 +0530

    pp

--- js/main.js
+++ js/main.js
@@ -98,10 +98,30 @@
                 items:1
             },
             768:{
-                items:2
+                items:1
             },
             992:{
-                items:3
+                items:1
+            }
+        }
+    });
+    $(".services-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 25,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:1
+            },
+            992:{
+                items:4
             }
         }
     });
