bcc4b2ae5cc4356bc60c03cb91cb221cb12ffe3d
Patternscompany <pandureddypatterns@gmail.com>
2025-02-23 00:27:41 +0530

    pp

--- js/main.js
+++ js/main.js
@@ -125,6 +125,26 @@
             }
         }
     });
+    $(".brands-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 25,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:5
+            }
+        }
+    });
     
 })(jQuery);
 
